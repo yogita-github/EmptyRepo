2cf6c573567d16ce39b5af018b4840a83e974a8c
Mahajan Yogita Vinayak <103029645+yogita-github@users.noreply.github.com>
2025-04-30 16:32:12 +0530

    Update 2_build.groovy

--- JavaAppProject/Jenkinsfiles/2_build.groovy
+++ JavaAppProject/Jenkinsfiles/2_build.groovy
@@ -1,11 +1,9 @@
 pipeline {
     agent any
     stages {
-        stage('Build') {
-            steps {
-                sh 'pwd' // Print current directory to check where you are
-                sh 'ls -R' // List all files to check if scripts/build.sh is there
-                sh 'chmod +x scripts/build.sh && scripts/build.sh'
+       stage('Build') {
+        steps {
+            bat 'echo Building on Windows...'
             }
         }
     }
