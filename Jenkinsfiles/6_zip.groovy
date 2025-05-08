7b24d74abf035ea98cce112f4140e6807a004ce2
Mahajan Yogita Vinayak <103029645+yogita-github@users.noreply.github.com>
2025-05-08 13:45:52 +0530

    Update 6_zip.groovy

--- Jenkinsfiles/6_zip.groovy
+++ Jenkinsfiles/6_zip.groovy
@@ -1,1 +1,24 @@
-pipeline { agent any; stages { stage('Zip') { steps { bat 'scripts/zip.bat' } } } }
+pipeline {
+    agent any
+    stages {
+        stage('Checkout SCM') {
+            steps {
+                checkout scm
+            }
+        }
+        stage('Debug') {
+            steps {
+                script {
+                    echo "Listing workspace contents:"
+                    bat 'dir'  // This will list the contents of the current directory in the workspace.
+                }
+            }
+        }
+        stage('Zip') {
+            steps {
+                bat 'scripts/zip.bat'  // Run your batch script after verifying directories
+            }
+        }
+    }
+}
+
