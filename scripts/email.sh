ff24afff60f4f5ea764207b0ea5e56a47da301e4
Mahajan Yogita Vinayak <103029645+yogita-github@users.noreply.github.com>
2025-05-08 14:36:41 +0530

    Update email.sh

--- scripts/email.sh
+++ scripts/email.sh
@@ -3,6 +3,6 @@ echo Pretend this sends an email with the following attachments:
 echo - reports\test_results.txt
 echo - build_output.zip
 echo.
-echo Email would be sent to: your-email@example.com
+echo Email would be sent to: yogitavm2001@gmail.com
 echo.
 pause
