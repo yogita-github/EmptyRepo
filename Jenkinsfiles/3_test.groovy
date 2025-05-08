5ca3f1f3da7eaea8d33e39efe7540c9d9e144ce2
Mahajan Yogita Vinayak <103029645+yogita-github@users.noreply.github.com>
2025-05-08 13:28:15 +0530

    Update 3_test.groovy

--- Jenkinsfiles/3_test.groovy
+++ Jenkinsfiles/3_test.groovy
@@ -1,1 +1,11 @@
-pipeline { agent any; stages { stage('Test') { steps { sh 'scripts/test.sh' } } } }
+// pipeline { agent any; stages { stage('Test') { steps { sh 'scripts/test.sh' } } } }
+pipeline {
+    agent any
+    stages {
+        stage('Test') {
+            steps {
+                bat 'echo Hello from batch'
+            }
+        }
+    }
+}
